9c5ef0e8e0a68290385cfb4a1204b254c3aeee95
Teriri <lsy1511736399@outlook.com>
2023-08-02 23:57:03 +0800

    订单服务层接口

--- src/main/java/com/example/bookmarket/service/IOrderService.java
+++ src/main/java/com/example/bookmarket/service/IOrderService.java
@@ -1,12 +1,18 @@
 package com.example.bookmarket.service;
 
-import com.example.bookmarket.dao.IOrderDao;
 import com.example.bookmarket.model.Order;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public interface IOrderService {
+    public Integer createOrder(Order order);
+
+    public Integer cancelOrder(Order order);
+
+    public Integer updateOrderStatue(Order order);
+
+    public Order searchOrder(String oid);
+
     public List<Order> getOrderList(String oid, String uid, String orderFilter, Integer page, Integer count);
 
     public Long getRecordsByOidAndUidAndStatus(String oid, String uid, String orderFilter);
