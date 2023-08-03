29ace3653bf61c907106fcaaebf27c79fcfd7c10
Teriri <lsy1511736399@outlook.com>
2023-08-03 22:35:02 +0800

    购物车控制层

--- src/main/java/com/example/bookmarket/controller/CartController.java
+++ src/main/java/com/example/bookmarket/controller/CartController.java
@@ -1,8 +1,10 @@
 package com.example.bookmarket.controller;
 
 import com.alibaba.fastjson2.JSON;
+import com.example.bookmarket.model.Address;
 import com.example.bookmarket.model.Cart;
 import com.example.bookmarket.model.User;
+import com.example.bookmarket.service.IAddressService;
 import com.example.bookmarket.service.ICartService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -19,16 +23,20 @@ public class CartController {
     private HttpSession session;
     @Autowired
     private ICartService cartService;
+    @Autowired
+    private IAddressService addressService;
 
     @RequestMapping(value = "/cartList", produces = "application/json;charset=utf-8", method = {RequestMethod.POST, RequestMethod.GET})
     public String getCartList() {
 //        String uid = ((User) session.getAttribute("user")).getUid();
-        String cartListJSON = JSON.toJSONString(cartService.getCartList("123456789"));
+        List<Cart> cartList = cartService.getCartList("123456789");
         Long selectedCartCount = cartService.getSelectedCartCount("123456789");
         Long totalCartCount = cartService.getTotalCartCount("123456789");
-        return "{\"cartList\":" + cartListJSON +
+        Address address = addressService.searchDefaultAddress("123456789");
+        return "{\"cartList\":" + JSON.toJSONString(cartList) +
                 ",\"selectedCartCount\":" + selectedCartCount +
-                ",\"totalCartCount\":" + totalCartCount + "}";
+                ",\"totalCartCount\":" + totalCartCount +
+                ",\"address\":" + JSON.toJSONString(address) + "}";
     }
 
     @RequestMapping(value = "/addCart", produces = "application/json;charset=utf-8", method = {RequestMethod.POST, RequestMethod.GET})
@@ -37,6 +45,17 @@ public class CartController {
         return cartService.addCart(cart);
     }
 
+    @RequestMapping(value = "/selectCart", produces = "application/json;charset=utf-8", method = {RequestMethod.POST, RequestMethod.GET})
+    public String selectCart(Cart cart) {
+        return cartService.updateCartSelect(cart);
+    }
+
+    @RequestMapping(value = "/selectAllCart", produces = "application/json;charset=utf-8", method = {RequestMethod.POST, RequestMethod.GET})
+    public String selectAllCart(Cart cart) {
+//        String uid = ((User) session.getAttribute("user")).getUid();
+        cart.setUid("123456789");
+        return cartService.updateAllCartSelect(cart);
+    }
     @RequestMapping(value = "/addCartCount", produces = "application/json;charset=utf-8", method = {RequestMethod.POST, RequestMethod.GET})
     public String addCartCount(Cart cart) {
         return cartService.addCartCount(cart);
