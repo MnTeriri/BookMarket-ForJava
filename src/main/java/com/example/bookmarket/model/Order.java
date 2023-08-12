1813571b3c7ca86957276d847a3ae35be881c2f2
Teriri <lsy1511736399@outlook.com>
2023-08-12 16:17:01 +0800

    订单实体类

--- src/main/java/com/example/bookmarket/model/Order.java
+++ src/main/java/com/example/bookmarket/model/Order.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.ibatis.annotations.One;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
