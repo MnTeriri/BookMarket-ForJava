35bd8380bc444c57639d44e2345c2e617fa73c9b
Teriri <lsy1511736399@outlook.com>
2023-08-12 16:16:47 +0800

    订单明细数据访问层

--- src/main/java/com/example/bookmarket/dao/IOrderBookDao.java
+++ src/main/java/com/example/bookmarket/dao/IOrderBookDao.java
@@ -1,9 +1,20 @@
 package com.example.bookmarket.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.bookmarket.model.Order;
 import com.example.bookmarket.model.OrderBook;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IOrderBookDao extends BaseMapper<OrderBook> {
+    @Select("SELECT * FROM Order_Book WHERE oid=#{oid}")
+    @Results({
+            @Result(property = "bid", column = "bid", javaType = String.class, jdbcType = JdbcType.VARCHAR),
+            @Result(property = "book", column = "bid", one = @One(select = "com.example.bookmarket.dao.IBookDao.searchBookByBid"))
+    })
+    public List<OrderBook> getOrderDetail(String oid);
 }
