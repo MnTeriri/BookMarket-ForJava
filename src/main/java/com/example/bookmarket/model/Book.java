e191c3d822bd36ed27db02d58d4f372ddaf638e8
Teriri <lsy1511736399@outlook.com>
2023-08-12 16:14:56 +0800

    书籍实体类

--- src/main/java/com/example/bookmarket/model/Book.java
+++ src/main/java/com/example/bookmarket/model/Book.java
@@ -3,6 +3,7 @@ package com.example.bookmarket.model;
 import com.alibaba.fastjson2.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.example.bookmarket.util.ImageUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
