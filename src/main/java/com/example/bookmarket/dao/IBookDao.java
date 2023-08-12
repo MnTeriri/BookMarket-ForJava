0b2c631d5d37d6c72ce008813422ba92f0bff2e9
Teriri <lsy1511736399@outlook.com>
2023-08-12 16:16:17 +0800

    书籍数据访问层

--- src/main/java/com/example/bookmarket/dao/IBookDao.java
+++ src/main/java/com/example/bookmarket/dao/IBookDao.java
@@ -2,9 +2,17 @@ package com.example.bookmarket.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.bookmarket.model.Book;
+import com.example.bookmarket.util.ImageUtils;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IBookDao extends BaseMapper<Book> {
+    @Select("SELECT * FROM Book WHERE bid=#{bid}")
+    @Results({
+            @Result(property = "imageString", column = "image", typeHandler = ImageUtils.class)
+    })
+    public Book searchBookByBid(String bid);
 }
