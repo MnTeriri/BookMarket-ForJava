15996101cbf714f1add8bf60be3adf895a90c5bc
Teriri <lsy1511736399@outlook.com>
2023-08-16 00:10:08 +0800

    存储数据访问层

--- src/main/java/com/example/bookmarket/dao/IStorageDao.java
+++ src/main/java/com/example/bookmarket/dao/IStorageDao.java
@@ -1,9 +1,12 @@
 package com.example.bookmarket.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.bookmarket.config.RedisCache;
 import com.example.bookmarket.model.Storage;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@CacheNamespace(implementation = RedisCache.class)
 public interface IStorageDao extends BaseMapper<Storage> {
 }
