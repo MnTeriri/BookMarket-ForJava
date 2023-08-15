84785413e15b91875ed4bb47c57153d806e5a928
Teriri <lsy1511736399@outlook.com>
2023-08-16 00:09:33 +0800

    订单明细数据访问层

--- src/main/java/com/example/bookmarket/dao/IOrderBookDao.java
+++ src/main/java/com/example/bookmarket/dao/IOrderBookDao.java
@@ -1,6 +1,7 @@
 package com.example.bookmarket.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.bookmarket.config.RedisCache;
 import com.example.bookmarket.model.Order;
 import com.example.bookmarket.model.OrderBook;
 import org.apache.ibatis.annotations.*;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@CacheNamespace(implementation = RedisCache.class)
 public interface IOrderBookDao extends BaseMapper<OrderBook> {
     @Select("SELECT * FROM Order_Book WHERE oid=#{oid}")
     @Results({
