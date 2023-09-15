dc82fb452107033de044453c1d53448e4c978d46
Teriri <1511736399@qq.com>
2023-09-15 11:30:52 +0800

    Redis配置类

--- src/main/java/com/example/bookmarket/config/RedisConfig.java
+++ src/main/java/com/example/bookmarket/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.bookmarket.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Slf4j
 @Configuration
 @EnableCaching
 public class RedisConfig {
+    public RedisConfig() {
+        log.debug("创建配置类对象：RedisConfig");
+    }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
