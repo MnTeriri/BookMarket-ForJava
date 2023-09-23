8407dbb6e293f001e7c83eedec9e07dd23ffb8c6
Teriri <1511736399@qq.com>
2023-09-23 22:43:38 +0800

    登录控制层

--- bookmarket-passport/src/main/java/com/example/bookmarketpassport/controller/LoginController.java
+++ bookmarket-passport/src/main/java/com/example/bookmarketpassport/controller/LoginController.java
@@ -1,14 +1,8 @@
 package com.example.bookmarketpassport.controller;
 
-import cn.hutool.crypto.digest.DigestUtil;
-import com.example.bookmarketpassport.model.LoginUser;
 import com.example.bookmarketpassport.model.User;
 import com.example.bookmarketpassport.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
