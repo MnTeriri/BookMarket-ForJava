a52c13747767830ed6ac595cb33923a10e67b8de
Teriri <1511736399@qq.com>
2023-09-23 20:47:10 +0800

    UserDetailsService实现类

--- bookmarket-passport/src/main/java/com/example/bookmarketpassport/service/impl/UserDetailsServiceImpl.java
+++ bookmarket-passport/src/main/java/com/example/bookmarketpassport/service/impl/UserDetailsServiceImpl.java
@@ -1,9 +1,10 @@
-package com.example.bookmarketpassport.service;
+package com.example.bookmarketpassport.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.bookmarketpassport.dao.IUserDao;
 import com.example.bookmarketpassport.model.LoginUser;
 import com.example.bookmarketpassport.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,15 +14,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Slf4j
 @Service
-public class UserService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private IUserDao userDao;
+
+    public UserDetailsServiceImpl() {
+        log.debug("创建自定义UserDetailsService对象：UserDetailsServiceImpl");
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("uid", username);
+        log.debug("用户{}信息查询成功", username);
         User user = userDao.selectOne(queryWrapper);
         user.setPassword("{bcrypt}" + new BCryptPasswordEncoder().encode(user.getPassword()));
         System.out.println("userMsg:" + user);
