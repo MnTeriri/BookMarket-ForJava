df60e68dc64d673dc864ceddae970e14ed2a048e
Teriri <1511736399@qq.com>
2023-09-23 22:42:43 +0800

    用户服务层实现

--- bookmarket-passport/src/main/java/com/example/bookmarketpassport/service/impl/UserServiceImpl.java
+++ bookmarket-passport/src/main/java/com/example/bookmarketpassport/service/impl/UserServiceImpl.java
@@ -4,14 +4,17 @@ import cn.hutool.crypto.digest.DigestUtil;
 import com.example.bookmarketpassport.model.LoginUser;
 import com.example.bookmarketpassport.model.User;
 import com.example.bookmarketpassport.service.IUserService;
+import com.example.bookmarketpassport.utils.RedisUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
-
+@Slf4j
 @Service
 public class UserServiceImpl implements IUserService {
     @Autowired
@@ -21,11 +24,9 @@ public class UserServiceImpl implements IUserService {
     public String login(User user) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUid(), DigestUtil.md5Hex(user.getPassword()));
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
-        if (Objects.isNull(authenticate)) {
-            throw new RuntimeException("用户名或密码错误");
-        }
         LoginUser loginUser = (LoginUser) authenticate.getPrincipal();
-        System.out.println("loginUser:" + loginUser);
+        RedisUtils.setCacheObject(user.getUid(), loginUser);
+        log.debug("登录信息存放到Redis：{}", loginUser);
         return null;
     }
 
