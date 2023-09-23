4ec06cabada2697e158b9aa9310ae646c67906b1
Teriri <1511736399@qq.com>
2023-09-23 22:41:50 +0800

    JWT Filter

--- bookmarket-passport/src/main/java/com/example/bookmarketpassport/fitter/JwtAuthenticationTokenFilter.java
+++ bookmarket-passport/src/main/java/com/example/bookmarketpassport/fitter/JwtAuthenticationTokenFilter.java
@@ -1,10 +1,15 @@
 package com.example.bookmarketpassport.fitter;
 
+import com.example.bookmarketpassport.model.LoginUser;
+import com.example.bookmarketpassport.utils.RedisUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,13 +18,24 @@ import java.io.IOException;
 @Slf4j
 @Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
     public JwtAuthenticationTokenFilter() {
         log.debug("创建配置类对象：JwtAuthenticationTokenFilter");
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println(111111111);
+        //判定header里面的token
+        //没token放行
+        //从token解析uid，查用户权限信息
+        String uid = "123456789";
+        LoginUser loginUser = RedisUtils.getCacheObject(uid);
+        if (loginUser == null) {
+            log.error("没有用户：{}登录信息，不允许访问", uid);
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, loginUser.getUser().getPassword(), loginUser.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);//添加到SecurityContext上下文
+        log.debug("用户：{}登录信息存在，允许访问，登录信息存放到SecurityContextHolder：{}", uid, authenticationToken);
         filterChain.doFilter(request, response);
     }
 }
